828d2848502022aa909b9d8a35e4a1fe86fabe78
shinhyocheol <zzzskssk1@naver.com>
2021-09-17 18:38:12 +0900

    2021.09.17(금)
     * 트랜잭션 어노테이션 동작 원리에 대한 공부
     * Datasource 생성부터 트랜잭션 매니저 설정까지의 순서를 파악하기 위해 로그 찍어보기

--- src/main/java/com/replication/app/api/product/service/ProductService.java
+++ src/main/java/com/replication/app/api/product/service/ProductService.java
@@ -5,6 +5,7 @@ import java.util.stream.Collectors;
 
 import com.replication.app.api.product.dto.ProductRegistDto;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -15,6 +16,7 @@ import com.replication.app.config.model.CustomModelMapper;
 
 @Service("productService")
 @RequiredArgsConstructor
+@Slf4j
 public class ProductService {
 
 	private final CustomModelMapper modelMapper;
@@ -23,15 +25,15 @@ public class ProductService {
 	
 	@Transactional(readOnly = true)
 	public List<ProductResultDto> getProducts() {
-
+		log.debug("트랜잭션을 시작합니다.");
 		List<Product> entityList = productRepository.findAll();
 
+		log.debug("트랜잭션을 종료합니다.");
 		return entityList.stream()
 				.map(entity -> modelMapper.toMapping(entity, ProductResultDto.class))
 				.collect(Collectors.toList());
 	}
 
-	@Transactional
 	public void regProduct(ProductRegistDto registDto) {
 
 		productRepository.save(registDto.toEntity());
--- src/main/java/com/replication/app/config/database/DatabaseConfig.java
+++ src/main/java/com/replication/app/config/database/DatabaseConfig.java
@@ -5,6 +5,7 @@ import java.util.LinkedHashMap;
 
 import java.util.Map;
 
+import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -23,6 +24,8 @@ import com.zaxxer.hikari.HikariDataSource;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
 
 @EnableJpaAuditing
 @Configuration
@@ -40,6 +43,7 @@ public class DatabaseConfig {
 	@Bean
 	@ConfigurationProperties(prefix = "datasource.master")
 	public DataSource masterDataSource() {
+		log.debug("Master DataSource 객체 생성");
 		return DataSourceBuilder.create().type(HikariDataSource.class).build();
 	}
 	
@@ -50,6 +54,7 @@ public class DatabaseConfig {
 	@Bean
 	@ConfigurationProperties(prefix = "datasource.slave")
 	public DataSource slaveDataSource() {
+		log.debug("slave DataSource 객체 생성");
 		return DataSourceBuilder.create().type(HikariDataSource.class).build();
 	}
 
@@ -69,6 +74,7 @@ public class DatabaseConfig {
 			@Qualifier("masterDataSource") DataSource masterDataSource,
 			@Qualifier("slaveDataSource") DataSource slaveDataSource) {
 
+		log.debug("Datasource Replication 작업 시작");
 		ReplicationRoutingDataSource replicationRoutingDataSource = new ReplicationRoutingDataSource();
 
 		Map<Object, Object> dataSourceMap = new LinkedHashMap<>();
@@ -78,6 +84,7 @@ public class DatabaseConfig {
 		replicationRoutingDataSource.setTargetDataSources(dataSourceMap);
 		replicationRoutingDataSource.setDefaultTargetDataSource(masterDataSource);
 
+		log.debug("Datasoruce Replication 작업 종료");
 		return replicationRoutingDataSource;
 	}
 
@@ -87,14 +94,33 @@ public class DatabaseConfig {
 	 * 		DataSource를 정하는 로직을 가능하게 하기 위해서는 쿼리를 실행하는 시점에서 DataSource를 
 	 * 		각 서비스별로 명시된 트랜잭션 타입을 확인 후 어떤 DataSource를 사용할지 정해야하는데
 	 * 		정하기전까지 DataSource 연결 시간을 늦추기 위해 LazyConnectionDataSourceProxy를 사용한다.
-	 * 
+	 *
+	 * @LazyConnectionDataSourceProxy : 실제 JDBC 연결을 하고자하는 Datasource에 대한 프록시이다.
+	 * 자동 커밋 모드, 트랜잭션 격리 및 읽기 전용모드와 같은 연결 초기화 속성은 실제 연결을 가져오는 즉시 실제 JDBC연결에 유지되고,
+	 * 적용된다. 결과적으로 트랜잭션을 시작하는 명령이 없다면 JDBC 연결을 시도하지 않으며, 커밋 및 롤백에 대한 호출이 무시된다.
+	 * 이 Datasource 프록시를 사용하면 실제로 필요한 경우가 아니면 풀에서 JDBC 연결을 가져오는것을 방지할 수 있다.
+	 * JDBC 트랜잭션 제어는 풀에서 연결을 가져오거나 데이터베이스와 통신하지 않고도 발생할 수 있다. 이 프록시는 JDBC 연결을 시도할 때
+	 * 지연 수행한다. 이 프록시는 일반 트랜잭션 경계 환경에서 특히 유용하다. 실제 데이터 엑세스가 발생하지 않는 경우 성능 저하 없이
+	 * 데이터 엑세스를 잠재적으로 수행할 수 있는 모든 방법에 대한 트랜잭션을 구분할 수 있다.
+	 *
 	 * @param routingDataSource
 	 * @return
 	 */
 	@Primary
 	@Bean
-	public DataSource dataSource(@Qualifier("routingDataSource") DataSource routingDataSource) {
+	public DataSource lazyRoutingDataSource(@Qualifier("routingDataSource") DataSource routingDataSource) {
+		log.debug("Datasource를 호출하기 전 어떤 Datasource를 호출할 지 결정하기까지 잠시 시간을 지연시키는중입니다...");
 		return new LazyConnectionDataSourceProxy(routingDataSource);
+	}
+
+	@Bean
+	public PlatformTransactionManager transactionManager(@Qualifier(value = "routingDataSource") DataSource routingDataSource) {
+		log.debug("트랜잭션 매니저에게 사용할 데이터 소스를 전달시작 합니다.");
+		JpaTransactionManager tm = new JpaTransactionManager();
+		tm.setDataSource(routingDataSource);
+
+		log.debug("트랜잭션 매니저에게 사용할 데이터 소스를 전달완료 하였습니다.");
+		return tm;
 	}
 
 
